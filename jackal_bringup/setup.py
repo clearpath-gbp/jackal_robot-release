317f65ab1220a55f3d03c0d4f92575dd5a821574
Mike Purvis <mpurvis@clearpathrobotics.com>
2015-02-03 14:07:07 -0500

    Trimmed the branch to only the jackal_robot sub directory

--- jackal_bringup/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup(**generate_distutils_setup(
-    packages=['jackal_bringup'],
-    package_dir={'': 'src'}
-))
